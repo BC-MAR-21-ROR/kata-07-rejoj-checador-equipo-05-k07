6585f0861a8496792541f40c84b80d6c2bba5c1e
Brayan Lopez <b.lopez@opensky.com.co>
2021-04-19 09:00:43 -0500

    Add relations, validation and scopes for AccessPoint model

--- app/models/access_point.rb
+++ app/models/access_point.rb
@@ -20,4 +20,10 @@
 class AccessPoint < ApplicationRecord
   enum status: { in: 0, out: 1 }
   belongs_to :employee
+
+  validates :check, :employee_id, presence: true
+
+  scope :by_date, -> (date) { where(check: date) }
+  scope :by_month, -> (date) { where(check: date.beginning_of_month..date.end_of_month) }
+  scope :by_employee, -> (employee_id) { where(employee_id: employee_id) }
 end
