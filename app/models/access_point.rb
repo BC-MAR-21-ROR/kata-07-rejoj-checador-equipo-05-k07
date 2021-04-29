cd51798dc948d9e99d7f5f77303b224f63a92942
Brayan Lopez <b.lopez@opensky.com.co>
2021-04-28 23:44:00 -0500

    add the mvc for AccessPoint

--- app/models/access_point.rb
+++ app/models/access_point.rb
@@ -26,4 +26,20 @@ class AccessPoint < ApplicationRecord
   scope :by_date, -> (date) { where(check: date) }
   scope :by_month, -> (date) { where(check: date.beginning_of_month..date.end_of_month) }
   scope :by_employee, -> (employee_id) { where(employee_id: employee_id) }
+
+  def self.did_checkin?(employee_id)
+    self.where("access_points.check BETWEEN ? AND ? AND status = 0 AND employee_id = ?",
+      Date.today.beginning_of_day,
+      Date.today.end_of_day,
+      employee_id)
+      .present?
+  end
+
+  def self.did_checkout?(employee_id)
+    self.where("access_points.check BETWEEN ? AND ? AND status = 1 AND employee_id = ?",
+      Date.today.beginning_of_day,
+      Date.today.end_of_day,
+      employee_id)
+      .present?
+  end
 end
